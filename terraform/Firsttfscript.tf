9f5725e419465c473ddc81bd899f32f6fef9e7db
VishnuReddy0007 <vishnu@flexydial.in>
2022-10-29 11:33:49 +0530

    updating modified file

--- terraform/Firsttfscript.tf
+++ terraform/Firsttfscript.tf
@@ -1,7 +1,7 @@
 provider "aws"{
     region = "ap-south-1"
-  //  access_key = "AKIASI4GYADT4K62MZ74"
-  //  secret_key = "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"
+  //  access_key = "AKIA74"
+  //  secret_key = "xYKbAaRGsIPcdQDwPlmde"
 }
 
 resource "aws_instance"   "AWSEC2Instance"{
